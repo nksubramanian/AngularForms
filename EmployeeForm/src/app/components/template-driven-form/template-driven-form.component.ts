16bff0c0b9b72797ec170fe8141180962c85f982
subramanian <subramanian.enkay@gmail.com>
2024-07-14 10:55:53 +0530

    template driven form

--- EmployeeForm/src/app/components/template-driven-form/template-driven-form.component.ts
+++ EmployeeForm/src/app/components/template-driven-form/template-driven-form.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-template-driven-form',
-  standalone: true,
-  imports: [],
   templateUrl: './template-driven-form.component.html',
-  styleUrl: './template-driven-form.component.css'
+  styleUrls: ['./template-driven-form.component.css']
 })
 export class TemplateDrivenFormComponent {
   userDetails = {
@@ -22,9 +20,4 @@ export class TemplateDrivenFormComponent {
       console.log('Form data:', this.userDetails);
     }
   }
-
-
-
-
-
 }
