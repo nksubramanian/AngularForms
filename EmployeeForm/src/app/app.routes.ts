171dc3ccc817c766444fa5b44fae20266cab166c
subramanian <subramanian.enkay@gmail.com>
2024-07-14 10:53:14 +0530

    routes file modified

--- EmployeeForm/src/app/app.routes.ts
+++ EmployeeForm/src/app/app.routes.ts
@@ -3,25 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { TemplateDrivenFormComponent } from './components/template-driven-form/template-driven-form.component';
 import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
 
+const routes: Routes = [
+  {
+    path:'', component:TemplateDrivenFormComponent,pathMatch:'full'
+  },
+  {
+    path:'template-driven', component:TemplateDrivenFormComponent
+  },
+  {
+    path:'reactive-form', component:ReactiveFormComponent
+  },
+  {
+    path:'**', component:TemplateDrivenFormComponent,pathMatch:'full'
+  },
+];
 
-export const routes: Routes = [
-    {
-      path:'', component:TemplateDrivenFormComponent,pathMatch:'full'
-    },
-    {
-      path:'template-driven', component:TemplateDrivenFormComponent
-    },
-    {
-      path:'reactive-form', component:ReactiveFormComponent
-    },
-    {
-      path:'**', component:TemplateDrivenFormComponent,pathMatch:'full'
-    },
-  ];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
 
-  @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-  export class AppRoutingModule { }
-  
